ffa7a8359417a6515d1b2765b01efb8e414edb49
Denilson <denilson@mplus.software>
2021-02-25 09:22:21 +0700

    using crossbeam to process it in parallel

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,9 +1,8 @@
 use image::png::PNGEncoder;
 use image::ColorType;
 use num::Complex;
-use std::io::Write;
+use std::fs::File;
 use std::str::FromStr;
-use std::{fs::File, io::stderr};
 
 fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     let mut z = Complex { re: 0.0, im: 0.0 };
@@ -137,6 +136,26 @@ fn main() {
     let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
     let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
     let mut pixels = vec![0; bounds.0 * bounds.1];
-    render(&mut pixels, bounds, upper_left, lower_right);
+
+    // render(&mut pixels, bounds, upper_left, lower_right);
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right =
+                    pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+                spawner.spawn(move || {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        });
+    }
+
     write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
